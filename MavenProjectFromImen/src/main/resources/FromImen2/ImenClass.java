d60fbaf3545cf145272437d7bf2fd6f6f442f709
jiheneA <jihenee.abdelhedi@gmail.com>
2017-07-07 08:56:17 +0100

    it's jihen

--- MavenProjectFromImen/src/main/resources/FromImen2/ImenClass.java
+++ MavenProjectFromImen/src/main/resources/FromImen2/ImenClass.java
@@ -3,6 +3,6 @@ package FromImen2;
 
 public class ImenClass {
     System.out.println("Hello world !!");
-    System.out.println("Hello world from Sifast!!");
+    System.out.println("Hello world from Sifast!! it's jihen");
 
 }
